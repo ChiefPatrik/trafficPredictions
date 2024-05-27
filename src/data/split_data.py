ed847b7ab50cb7cb4b33b2adc0350dd46d565147
Patrik Praprotnik <praprotnik.patrik@gmail.com>
2024-05-27 21:22:24 +0200

    Added script for spliting data into train and test datasets

---
+++ src/data/split_data.py
@@ -0,0 +1,25 @@
+import os
+import pandas as pd
+from sklearn.model_selection import train_test_split
+
+current_dir = os.path.dirname(os.path.abspath(__file__))
+data_dir = os.path.join(current_dir, '..', '..', 'data')
+merged_data_path = os.path.join(data_dir, 'merged')
+
+# Iterate over all files in merged directory
+for filename in os.listdir(merged_data_path):
+    if filename.endswith('_data.csv'):
+        # Read the file into a DataFrame
+        df = pd.read_csv(os.path.join(merged_data_path, filename))
+
+        # Split the DataFrame into train and test sets
+        train, test = train_test_split(df, test_size=0.1, random_state=1234)
+
+        # Define the base of the filename (without extension and up until '_')
+        base_filename = filename.split('.')[0].split('_')[0]
+
+        # Save the train set as 'X_train.csv' and the test set as 'X_test.csv'
+        train.to_csv(os.path.join(merged_data_path, f'{base_filename}_train.csv'), index=False)
+        test.to_csv(os.path.join(merged_data_path, f'{base_filename}_test.csv'), index=False)
+
+print("Data splited and saved successfully for all regions!")
